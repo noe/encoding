d247b345adc0156e5b4faefd52c4ecb57dcd671e
Noe Casas <noe.casas@gmail.com>
2013-07-10 01:40:47 +0200

    Change namespace

--- src/test.cpp
+++ src/test.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(void)
 {
-  genalg::UnsignedIntegerCodec caca(16);  
+  encoding::UnsignedIntegerCodec caca(16);  
   cout << caca.encode(5) <<endl;
 
-  genalg::EnumCodec<string> codec({"hola","adios"}, "hola");
+  encoding::EnumCodec<string> codec({"hola","adios"}, "hola");
   cout << codec.encode("hola") << endl;
   cout << codec.decode(boost::dynamic_bitset<>(1, 0)) << endl;
   return 0;
