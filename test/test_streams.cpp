e195a8f7e39e89b6b5e1c1bcb27fb9e739aa63ee
Noe Casas <noe.casas@gmail.com>
2013-08-28 22:46:42 +0200

    Add unit test for streams

--- test/test_streams.cpp
+++ test/test_streams.cpp
@@ -5,18 +5,19 @@
 #include "encoding/input.hpp"
 #include "encoding/output.hpp"
 
+#include <gtest/gtest.h>
 
-void testInputStream ()
+TEST(TestInputStream, SimpleTest)
 {
   std::istringstream in({0x01,0x25,0x23,0x53});
   encoding::InputStream input(in);
 
   encoding::UnsignedIntegerCodec c(16);  
-  std::cout << "paco: " << input.read(c) << std::endl;
-  std::cout << "paco: " << input.read(c) << std::endl;
+  ASSERT_EQ(input.read(c), 0x0125);
+  ASSERT_EQ(input.read(c), 0x2353);
 }
 
-void testOutputStream ()
+TEST(testOutputStream, SimpleTest)
 {
   encoding::UnsignedIntegerCodec c5(8);  
   encoding::UnsignedIntegerCodec c6(4);  
@@ -28,5 +29,5 @@ void testOutputStream ()
   output.write(c6, 6U);
   output.write(c6, 4U);
   output.close();
-  std::cout << "miau: " << out.str() << std::endl;
+  ASSERT_EQ(out.str(), std::string("abcd"));
 }
